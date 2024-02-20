f4ffcd67658bce4b5570cb7c378a576fd1e66e38
Shubho0033 <hellskitchen1869@gmail.com>
2024-02-20 19:41:57 +0530

    Initial Commit

---
+++ home/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import BlogModel,Profile
+
+
+admin.site.register(BlogModel)
+
+admin.site.register(Profile)  # Register the Profile model with Django admin
